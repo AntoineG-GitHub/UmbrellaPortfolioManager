05cea1e6d5b6c54ef5f6ca5d478a79b9a541750b
AntoineG-GitHub <antoinegeller@hotmail.be>
2024-01-17 18:06:33 +0100

    delete pydantic + working code

--- umbrella_portfolio_manager/umbrella_portfolio_manager/portfolio/stock.py
+++ umbrella_portfolio_manager/umbrella_portfolio_manager/portfolio/stock.py
@@ -1,27 +1,18 @@
-from pydantic import AnyUrl, BaseModel, PrivateAttr
-from datetime import date
 import yfinance as yf
-class Stock(BaseModel):
-    """Class that gathers informatoin about the stock present in the portfolio; 
+
+
+class Stock:
+    """Class that gathers information about the stock present in the portfolio; 
     Information is translated by the stock price history, the quantity, the buying dates, etc. 
     """
-    ticker: str
-    stock: yf.Ticker = PrivateAttr(default=None)
-    hello: str
-    class Config:
-        # Ajoutez cette configuration pour autoriser les types arbitraires
-        arbitrary_types_allowed = True
-        extra = "allow"  # Permet d'ignorer les champs non déclarés dans le modèle
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.stock = yf.Ticker(ticker)
         self.load_metadata()
-        self.stock = yf.Ticker(self.ticker)
-        # self.stock_history = self.load_price_history(self.ticker)
 
     def load_metadata(self):
-        
-        self.hello = "hello"
+        self.actions = self.stock.actions
 
     def load_price_history(ticker, date_start, period):
         pass
