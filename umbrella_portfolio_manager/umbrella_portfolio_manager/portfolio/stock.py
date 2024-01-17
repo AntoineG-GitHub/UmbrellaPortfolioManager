4a1ead80c29ae35e442bc77ab2c6bb7ecb683831
AntoineG-GitHub <antoinegeller@hotmail.be>
2024-01-17 14:04:06 +0100

    setup three function + notebook

--- umbrella_portfolio_manager/umbrella_portfolio_manager/portfolio/stock.py
+++ umbrella_portfolio_manager/umbrella_portfolio_manager/portfolio/stock.py
@@ -1,6 +1,22 @@
 from pydantic import BaseModel
+from datetime import date
 
 class Stock(BaseModel):
-    symbol: str
-    quantity: int
-    price_per_share: float
+    """Class that gathers informatoin about the stock present in the portfolio; 
+    Information is translated by the stock price history, the quantity, the buying dates, etc. 
+    """
+    ticker: str
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.stock_history = self.load_price_history(self.ticker)
+
+    def load_metadata(ticker):
+        pass
+
+    def load_price_history(ticker, date_start, period):
+        pass
+
+    def add_transaction(buying_date, buying_price, quantity):
+        pass
+
