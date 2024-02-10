bab7debbe3bad17667a762ae8b49ef35037ee1ec
AntoineG-GitHub <antoinegeller@hotmail.be>
2024-02-10 13:52:54 +0100

    recent functions portfolio.py

--- umbrella_portfolio_manager/umbrella_backend/backend/umbrella_portfolio_manager/portfolio/portfolio.py
+++ umbrella_portfolio_manager/umbrella_backend/backend/umbrella_portfolio_manager/portfolio/portfolio.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from portfolio.holders import Holders 
+
+from umbrella_portfolio_manager.umbrella_portfolio_manager.portfolio.holders import Holders
+from umbrella_portfolio_manager.umbrella_portfolio_manager.portfolio.stock import Stock 
 
 class Portfolio:
     
@@ -8,24 +10,20 @@ class Portfolio:
         self.total_value = 0
         self.holders = {}
         self.portfolio = {}
-        self.stock_transactions = pd.DataFrame(columns=['ticker', 'transactions_date', 'quantity', 
+        self.stocks = {}
+        self.portfolio_transactions = pd.DataFrame(columns=['ticker', 'transactions_date', 'quantity', 
                                                         'transaction_price', 'conversation_rate', 
                                                         'transaction_price_euro', 'charge'])
         
-    def load_total_value(self):
-        pass
+    def update_total_value(self):
+        for value in self.stocks.values() :
+            self.total_value =+ value.quantity*value.minute_price        
 
     def update_portfolio(self, ticker: str, quantity: int): 
         if ticker in self.portfolio.keys():
            self.portfolio[ticker] += quantity
         else :
             self.portfolio[ticker]= quantity
-
-    def update_total_value(self):
-        pass
-
-    def load_holders_benefits(self):
-        pass
 
     def update_holders_benefits(self):
         pass
@@ -42,11 +40,39 @@ class Portfolio:
 
     def add_stock_transactions(self, ticker: str, date, quantity: int, transaction_price: float,
                                conversation_rate: float, transaction_price_euro: float, charge: float):
+        if ticker in self.stocks.keys():
+            self.stocks[ticker].update_quantity(quantity)
+        else: 
+            self.stocks[ticker] = Stock(ticker = ticker, quantity = quantity)
         
-        # Call Stock function to update the whole object
         new_stocks = pd.DataFrame([{'ticker': ticker, 'transactions_date': date, 'quantity': quantity, 
                                                         'transaction_price': transaction_price, 'conversation_rate': conversation_rate, 
                                                         'transaction_price_euro': transaction_price_euro, 'charge':charge}])
-        self.stock_transactions = pd.concat([self.stock_transactions, new_stocks])
- 
+        self.portfolio_transactions = pd.concat([self.portfolio_transactions, new_stocks])
+        self.stocks[ticker].add_transaction(date, quantity, transaction_price,
+                               conversation_rate, transaction_price_euro, charge)
+       
         self.update_portfolio(ticker, quantity)
+
+    def update_all_history(self):
+        history_stocks = {}
+        periods = self.portfolio_transactions['transactions_date'].unique()
+        print(periods)
+        for index, row in self.portfolio_transactions.iterrows():
+            if row['ticker'] in history_stocks.keys():
+                history_stocks[row['ticker']].update_quantity(row['quantity'])
+            else: 
+                history_stocks[row['ticker']] = Stock(ticker = row['ticker'], quantity = row['quantity'])
+            history_stocks[row['ticker']].update_dict_evolution_historic(date_start = row['transactions_date'], date_end = '2024-01-01')
+        print(index, row)
+
+        for date in periods: 
+            pass
+        # pour chaque date dans les periodes, on recrée une table 
+        # d'historique pour chaque stock de la transaction 
+        
+
+    def update_portfolio_evolution(self):
+        pass
+
+    
